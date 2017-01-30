22552f72cfff922da012a7f4b155d9ef87458d3e
Jim Baker <jim.baker1217@gmail.com>
2017-01-29 19:56:03 -0600

    studio 4 commit

--- main.py
+++ main.py
@@ -56,8 +56,9 @@ class AddMovie(webapp2.RequestHandler):
 
         # TODO 1
         # Use a template to render the confirmation message
-
-        self.response.write("Under construction...")
+        t = jinja_env.get_template("add-movie.html")
+        content = t.render(new_movie_escaped=new_movie_escaped)
+        self.response.write(content)
 
 
 class CrossOffMovie(webapp2.RequestHandler):
